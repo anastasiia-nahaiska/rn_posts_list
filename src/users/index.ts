ca6a332bba79cad22432006a2fdfda055f3cbb1d
Anastasiia Nahaiska <nahaiskaanastasiia@gmail.com>
2022-12-25 15:50:01 +0200

    changed users

--- src/users/index.ts
+++ src/users/index.ts
@@ -2,5 +2,4 @@ export const users = [
   { email: 'taras_shevchenko@ukr.net', password: '13mynalo' },
   { email: 'lesia_ukrainka@ukr.net', password: 'contraspemspero' },
   { email: 'h_skovoroda@gmail.com', password: 'filosof1722' },
-  { email: 'Q', password: 'Q' },
 ];
